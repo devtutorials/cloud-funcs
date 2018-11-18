1b57b3d8f523fe73a7e3a200f40cd02c731aeeff
Matthew Jeng <mjeng@berkeley.edu>
2018-11-17 18:02:52 -0800

    cloud function multiple request to API

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 'use strict';
 
-const Buffer = require('safe-buffer').Buffer;
-const escapeHtml = require('escape-html');
+// const Buffer = require('safe-buffer').Buffer;
+// const escapeHtml = require('escape-html');
 
 /* MY STUFF */
 exports.actuator = (req, res) => {
+
   var http = require("http");
 
+  var numReqs = 10;
   var rateLimit = "1/s";
+  // var hostname = ;
+
   var options = {
     "method": "GET",
     "hostname": "35.225.71.61",
     "path": "/users?rateLimit=" + rateLimit
   };
+  var actReq;
+  var responses = new Array();
+  for (var i = 0; i < numReqs; i++) {
+    actReq = http.request(options, function (actRes) {
+      var responseString = "";
 
-  var actReq = http.request(options, function (actRes) {
-    var responseString = "";
+      actRes.on("data", function (data) {
+        responseString += data;
+      });
 
-    actRes.on("data", function (data) {
-      responseString += data;
+      actRes.on("end", function () {
+        responses.push(responseString);
+        if (responses.length == numReqs) {
+          res.send(responses.join("\n\n") + "LENGTH: " + responses.length.toString());
+        }
+      });
     });
+    actReq.write("");
+    actReq.end();
+  }
 
-    actRes.on("end", function () {
-      res.send(responseString);
-    });
-  });
-
-  actReq.write("");
-  actReq.end();
+  // res.send("done");
 }
 
 exports.test = (req, res) => {
@@ -43,5 +54,5 @@ exports.test = (req, res) => {
 }
 
 exports.test2 = (req, res) => {
-
+  res.send(req.param['someprop']);
 }
